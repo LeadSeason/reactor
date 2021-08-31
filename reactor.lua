1ab36c218f5de266bbd062cd045234180b3c3cf6
leadseason <pauli.tiittanen113@gmail.com>
2021-08-31 17:28:14 +0300

    a

--- reactor.lua
+++ reactor.lua
@@ -1,22 +1,13 @@
 local component = require("component")
 local reactor = component.br_reactor
 
-local last_str = ""
-function iop(str)
-    io.write(('\b \b'):rep(#last_str))  -- erase old line
-    io.write(str)                       -- write new line
-    io.flush()
-    last_str = str
-end
-
 reactor.setActive(true)
 
 local n = 60
 
 while (n >= 1) do
-    iop(tostring(n) + " | " + tostring(reactor.getEnergyStored()))
+    print(tostring(reactor.getEnergyStored()))
     os.sleep(1)
-    n = n-1
 end
 
 reactor.setActive(false)
